d03752664e537cafabab0f28056e73fab4b49ca2
Wibumoe <131719695+wibumoe@users.noreply.github.com>
2024-10-01 22:53:40 +0700

    Update index.js

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const dev = process.env.NODE_ENV !== "production";
 
 export const server = dev
   ? "http://localhost:3000"
-  : "https://komik-two.vercel.app";
+  : "https://komik-tau.vercel.app";
 
 
 // export const API = dev ? 'http://localhost:3000' : ''
